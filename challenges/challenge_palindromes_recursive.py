d60fbf74a7c7a68960c1ac5f5a1ca20f1ece4740
Luciano AAP <lucianoalmeidaap@gmail.com>
2022-03-18 16:02:51 -0300

    adiciona requisito 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,14 @@
+def reverse(word, low_index, high_index):
+    if low_index == high_index:
+        return word
+    word = word[0:low_index] + word[high_index] + word[low_index:high_index]
+    return reverse(word, low_index + 1, high_index)
+
+
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if word is None or word == '':
+        return False
+    reversed_word = reverse(word, low_index, high_index)
+    if reversed_word == word:
+        return True
+    return False
