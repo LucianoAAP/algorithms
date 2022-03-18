0ff14eb7a3fd917e18dd1ead1fb220d8e4a44750
Luciano AAP <lucianoalmeidaap@gmail.com>
2022-03-18 13:52:17 -0300

    tenta requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if target_time is None:
+        return None
+
+    count = 0
+
+    for period in permanence_period:
+        if (type(period) != tuple
+                or type(period[0]) != int or type(period[1]) != int):
+            return None
+        if period[0] <= target_time and period[1] >= target_time:
+            count += 1
+
+    return count
