d3af96ebc546f8af6087e0836f7008fe8daca9f0
Luciano AAP <lucianoalmeidaap@gmail.com>
2022-03-18 17:57:32 -0300

    adiciona requisito 5

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,9 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    if word == '':
+        return False
+    reversed_word = ''
+    for letter in word:
+        reversed_word = f'{letter}{reversed_word}'
+    if word == reversed_word:
+        return True
+    return False
